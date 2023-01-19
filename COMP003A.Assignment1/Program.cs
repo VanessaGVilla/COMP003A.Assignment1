3ca54aa0b07f3851171a7c11c76f45710c296ba0
vanessagarcia <vanessagarcia@LEM231-06>
2023-01-18 19:52:56 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,20 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment
+ * Author: Vanessa Garcia
+ * Course: COMP-003A-L01
+ * Purpose: Code for Assignment 1
+ */
+
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("")
+        }
+    }
+}
