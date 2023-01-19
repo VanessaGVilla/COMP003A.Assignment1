390369a76c9547459193e42cbfcedd8e691a05cc
VanessaGVilla <nessa619@msn.com>
2023-01-18 20:26:02 -0800

    additional commit

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -14,7 +14,30 @@ namespace COMP003A.Assignment1
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("100 Things About Me");
-            Console.WriteLine("")
+            Console.WriteLine("I'm a book nerd");
+            Console.WriteLine("My first language was Spanish");
+            Console.WriteLine("I was born and raised in San Diego");
+            Console.WriteLine("My parents were both immigrants");
+            Console.WriteLine("I have zero tattoos");
+            Console.WriteLine("I might yet finally get a tattoo");
+            Console.WriteLine("I view the ocean as an old friend");
+            Console.WriteLine("My first dslr camera was a Nikon D90");
+            Console.WriteLine("My first camera lens was a 50 mm");
+            Console.WriteLine("My second lens was a 35 mm");
+            Console.WriteLine("I once wrote a short story book of fiction as a teen");
+            Console.WriteLine("It won an award and received first place");
+            Console.WriteLine("I'm a Scorpio");
+            Console.WriteLine("My family nickname is pequitas");
+            Console.WriteLine("It translates to freckles");
+            Console.WriteLine("Halloween is my 2nd favorite time of year");
+            Console.WriteLine("I love cooking food from scratch");
+            Console.WriteLine("I prefer pinot noir, pinot grigio and cabernet sauvignon");
+            Console.WriteLine("Not a fan of sweet wine");
+            Console.WriteLine("I prefer beer in the Summer");
+            Console.WriteLine("I have three teenagers");
+            Console.WriteLine("I'm neither an introvert or extrovert");
+            Console.WriteLine("I prefer scary movies over romantic ones");
+
         }
     }
 }
